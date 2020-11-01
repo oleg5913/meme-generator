c5b52588740c409da1bfed42c887efcc214ff3a0
oleg5913 <oleg1906@gmail.com>
2020-11-01 20:31:17 +0300

    first commit

---
+++ precache-manifest.9791719b1d783ddbf98d09e9547b64cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868ae8224bfa67b4d036d6d2821b6843",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "d959ed27eddaf9c6c718",
+    "url": "/meme-generator/static/css/main.39d72d77.chunk.css"
+  },
+  {
+    "revision": "9c70bcd3b330fd3239b9",
+    "url": "/meme-generator/static/js/2.6b17ddbf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/meme-generator/static/js/2.6b17ddbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d959ed27eddaf9c6c718",
+    "url": "/meme-generator/static/js/main.bbe2fabf.chunk.js"
+  },
+  {
+    "revision": "c13a2d47a72ea9862028",
+    "url": "/meme-generator/static/js/runtime-main.3af06f96.js"
+  }
+]);
